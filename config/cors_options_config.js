8d87aa766649c6dbe026c46eec07cc34164a0df9
theblvckdev <nathancodes05@gmail.com>
2024-05-09 07:15:53 +0100

    cors options update

---
+++ config/cors_options_config.js
@@ -0,0 +1,14 @@
+const whiteList = ["http://localhost:3500", "http://localhost:8080"];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+module.exports = corsOptions;
