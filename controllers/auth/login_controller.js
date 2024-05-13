72174c963df2095faad0e3b960c62741f48cb339
theblvckdev <nathancodes05@gmail.com>
2024-05-13 13:50:52 +0100

    worked on authentication for user login

--- controllers/auth/login_controller.js
+++ controllers/auth/login_controller.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
 const bcrypt = require("bcrypt");
 const db = require("../../config/db_connection_config");
+// const signJWTtoekn = require("../../utils/sign_JWT");
+const jwt = require("jsonwebtoken");
 
 const logiAdmin = (req, res) => {
   const { email, password } = req.body;
@@ -10,7 +13,7 @@ const logiAdmin = (req, res) => {
       .status(401)
       .json({ status: "unauthorized", message: "All fields are required" });
 
-  const sqlQuery = "SELECT email, password FROM admins WHERE email = ?";
+  const sqlQuery = "SELECT * FROM admins WHERE email = ?";
   db.query(sqlQuery, [email], (err, data) => {
     if (err)
       return res
@@ -27,21 +30,34 @@ const logiAdmin = (req, res) => {
     const pwdMatch = bcrypt.compareSync(password, data[0].password);
 
     if (!pwdMatch)
-      return res
-        .status(401)
-        .json({
-          status: "Unauthorized",
-          message: "Invalid email address or password!",
-        });
+      return res.status(401).json({
+        status: "Unauthorized",
+        message: "Invalid email address or password!",
+      });
 
     // create JWT access token
+    const accessToken = jwt.sign(
+      { id: data[0].admin_id },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRATION,
+      }
+    );
 
-    res
-      .status(200)
-      .json({
-        status: "ok",
-        message: "Your logged into your account successfully.",
-      });
+    const refreshToken = jwt.sign(
+      { id: data[0].admin_id },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: process.env.JWT_REFRESH_TOKEN_EXPIRATION,
+      }
+    );
+
+    res.status(200).json({
+      status: "ok",
+      message: "Your logged into your account successfully.",
+      accessToken,
+      refreshToken,
+    });
   });
 };
 
